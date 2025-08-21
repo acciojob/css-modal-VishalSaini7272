5af135fcf3ffe9e6bef9f85e45cca286f16b115a
VishalSaini7272 <sainivishal156@gmail.com>
2025-08-22 00:05:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+//your JS code here. If requhttps://www.svgrepo.com/show/345221/three-dots.svgired.
+const modal= document.getElementByClass("modal")
+const openBtn = document.getElementById("openModal")
+const closeBtn = document.getElementByClass("close-modal")
+
+openBtn.onclick =function(){
+	modal.style.display ="flex";
+}
+
+closeBtn.onclick = function(){
+	modal.style.display = "none";
+}
+
+window.onclick = function(event){
+	if(event.target === modal){
+		modal.style.display ="none";
+	}
+}
